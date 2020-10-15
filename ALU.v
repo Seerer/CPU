4d6d55dab03d69e38435540ad4df7d64c5a5139f
SeerKing <3180104475@zju.edu.cn>
2020-10-15 11:12:03 +0800

    modified

--- ALU.v
+++ ALU.v
@@ -1,1 +1,17 @@
-module ALU ();
+module ALU (A, B, ALUCode, ALUResult);
+
+input [31:0] A, B;
+input [3:0] ALUCode;
+
+output [31:0] ALUResult;
+
+wire [31:0]Binvert;
+wire [31:0]sum;
+wire [31:0]input_B;
+
+assign Binvert = {32{~(ALUCode == 0)}};
+assign input_B = B^Binvert;
+
+adder32_bits adder2(.a(A), .b(input_B), .ci(Binvert), .co(), .s(sum));
+
+
