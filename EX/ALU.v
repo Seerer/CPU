f77fea4691414f3dfc0437c74efbffa8d22d7e75
Seerking <895290575@qq.com>
2020-10-16 00:03:35 +0800

    ALU modification

--- EX/ALU.v
+++ EX/ALU.v
@@ -8,10 +8,13 @@ output [31:0] ALUResult;
 wire [31:0]Binvert;
 wire [31:0]sum;
 wire [31:0]input_B;
+wire slt, sltu;
 
 assign Binvert = {32{~(ALUCode == 0)}};
 assign input_B = B^Binvert;
+assign slt = A[31] && (~B[31]) || (A[31]~^B[31]) && sum[31];
+assign sltu = (~A[31]) && B[31] || (A[31]~^B[31]) && sum[31];;
 
 adder32_bits adder2(.a(A), .b(input_B), .ci(Binvert), .co(), .s(sum));
-
+mux16_1 mux1(sum, sum, B, (A & B), (A ^ B), (A | B), 0, 0, 0, slt, sltu, 0, 0, 0, 0, 0, ALUCode, ALUResult);
 
